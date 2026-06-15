5d13198781143ad6bb76db698b5eefbe965a2b8e
Balthazzahr <Balthazzahr@users.noreply.github.com>
2026-06-15 14:02:37 +1000

    Auto-commit: local modifications sync

--- src/ui/icons.rs
+++ src/ui/icons.rs
@@ -44,5 +44,5 @@ pub const ICON_PLUS:     &str = "\u{f067}";  //
 pub const ICON_TRASH:    &str = "\u{f1f8}";  //
 pub const ICON_COPY:     &str = "\u{f0c5}";  //
 pub const ICON_PODIUM:   &str = "\u{f0d25}"; //
-pub const ICON_VISUALIZER: &str = "\u{f080}";  // bar-chart
-pub const ICON_LYRICS:     &str = "\u{f15c}";  // file-text
+pub const ICON_VISUALIZER: &str = "\u{f147d}"; // waveform
+pub const ICON_LYRICS:     &str = "\u{f0cb9}"; // lyrics custom
